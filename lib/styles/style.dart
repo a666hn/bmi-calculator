6f9b0900ad3b52022861194da048c8317b27b7de
A666HN <adidot78@gmail.com>
2021-12-21 22:03:06 +0700

    Create main themes

---
+++ lib/styles/style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData kMainMaterialTheme = ThemeData.dark().copyWith(
+  appBarTheme: const AppBarTheme(
+    color: Color(0xFF090C22),
+  ),
+  scaffoldBackgroundColor: const Color(0xFF090C22),
+);
